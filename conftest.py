a660bf70ce27e21aa978af163c97e14c9e3dd98e
bruno Henrique de almeida lima <blima@cpqd.com.br>
2026-03-04 00:15:21 -0300

    atualização .gitgnore e conftest

---
+++ conftest.py
@@ -0,0 +1,4 @@
+# Este arquivo conftest.py centraliza fixtures e configurações globais
+# do projeto de testes.  
+# Aqui você pode definir dados aleatórios, objetos, setups e hooks
+# que serão usados em vários testes sem precisar importar nada.
