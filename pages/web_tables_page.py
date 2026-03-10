33c31641f2d68f6d7653581f99e2627c16558c29
bruno Henrique de almeida lima <blima@cpqd.com.br>
2026-03-10 01:43:32 -0300

    incrementando teste excluir usuario de tabela

--- pages/web_tables_page.py
+++ pages/web_tables_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import expect
 from config.settings import NOME_USUARIO, EMAIL_USUARIO, CURRENT_ADDRESS, PERMANENT_ADDRESS
 
+#  Cenário: pesquisar dados na tabela
 def clicar_elements(page):
     page.get_by_text("Elements").click()
 
@@ -13,3 +14,15 @@ def pesquisar_tabela(page):
 def validar_dados_tabela(page):
     expect(page.get_by_role("cell", name="Cierra", exact=True)).to_be_visible()
  
+ #  Cenário: excluir dados da tabela
+def clicar_elements(page):
+    page.get_by_text("Elements").click()
+
+def abrir_web_tables(page):
+    page.get_by_text("Web Tables").click()
+
+def excluir_dados_tabela(page):
+    page.locator("#delete-record-1 > svg > path").click()
+
+def validar_exclusao_tabela(page):
+    expect(page.get_by_role("cell", name="Cierra", exact=True)).not_to_be_visible()
--- steps/web_tables_steps.py
+++ steps/web_tables_steps.py
@@ -1,8 +1,8 @@
 from pytest_bdd import when, then
-from pages.web_tables_page import abrir_web_tables, abrir_web_tables, clicar_elements, pesquisar_tabela, validar_dados_tabela   
+from pages.web_tables_page import abrir_web_tables, abrir_web_tables, clicar_elements, excluir_dados_tabela, excluir_dados_tabela, pesquisar_tabela, validar_dados_tabela, validar_exclusao_tabela, validar_exclusao_tabela   
 from utils.helpers import tirar_print
 
-
+#  Cenário: pesquisar dados na tabela
 @when('ele clica na opção Elements')
 def step_clicar_elements(page):
     clicar_elements(page)
@@ -20,3 +20,23 @@ def step_pesquisar_tabela(page):
 @then ('somente cierra deve aparecer na tabela')
 def step_validar_dados_tabela(page):
     validar_dados_tabela(page) 
+
+
+#  Cenário: excluir dados da tabela
+@when('ele clica na opção Elements')
+def step_clicar_elements(page):
+    clicar_elements(page)
+    tirar_print(page, "ele clica na opção Elements")
+
+@when('acessa a seção web tables')
+def step_abrir_web_tables(page):
+    abrir_web_tables(page)
+    tirar_print(page, "acessa a seção web tables")
+
+@when('clicar em delete na linha de cierra')
+def step_excluir_dados_tabela(page):
+    excluir_dados_tabela(page)
+
+@then('linha de cierra deve ser excluída da tabela')
+def step_validar_exclusao_tabela(page):
+    validar_exclusao_tabela(page)
