4462501f3d74ecf231f78415a38ae15c1e32c6fd
bruno Henrique de almeida lima <blima@cpqd.com.br>
2026-03-12 03:08:56 -0300

    finalização test links

--- pages/links_page.py
+++ pages/links_page.py
@@ -45,6 +45,88 @@ def clicar_created(page):
     page.get_by_role("link", name="Created").click()
 
 def validar_resposta_created(page):
-    expect(page.get_by_text("Link has responded with staus", exact=True)).to_be_visible()
-    expect(page.get_by_text("201", exact=True)).to_be_visible()
-    expect(page.locator("#linkResponse").get_by_text("Created", exact=True)).to_be_visible()
+    expect(page.locator("#linkResponse")).to_contain_text("Link has responded with staus 201 and status text Created")
+
+
+#   Cenário: Usuário clica no link no content
+def clicar_elements(page):
+    page.get_by_text("Elements").click()
+
+def abrir_links(page):
+    page.get_by_text("Links", exact=True).click()
+
+def clicar_no_content(page):
+    page.get_by_role("link", name="No Content").click()
+
+def validar_resposta_no_content(page):
+    expect(page.locator("#linkResponse")).to_contain_text("Link has responded with staus 204 and status text No Content")
+
+
+#   Cenário: Usuário clica no link moved
+def clicar_elements(page):
+    page.get_by_text("Elements").click()
+
+def abrir_links(page):
+    page.get_by_text("Links", exact=True).click()
+
+def clicar_moved(page):
+    page.get_by_role("link", name="Moved").click()
+
+def validar_resposta_moved(page):
+    expect(page.locator("#linkResponse")).to_contain_text("Link has responded with staus 301 and status text Moved Permanently")
+
+
+#   Cenário: Usuário clica no link bad request
+def clicar_elements(page):
+    page.get_by_text("Elements").click()
+
+def abrir_links(page):
+    page.get_by_text("Links", exact=True).click()
+
+def clicar_bad_request(page):
+    page.get_by_role("link", name="Bad Request").click()
+
+def validar_resposta_bad_request(page):
+   expect(page.locator("#linkResponse")).to_contain_text("Link has responded with staus 400 and status text Bad Request")
+
+
+#   Cenário: Usuário clica no link unauthorized
+def clicar_elements(page):
+    page.get_by_text("Elements").click()
+
+def abrir_links(page):
+    page.get_by_text("Links", exact=True).click()
+
+def clicar_unauthorized(page):    
+    page.get_by_role("link", name="Unauthorized").click()
+
+def validar_resposta_unauthorized(page):    
+    expect(page.locator("#linkResponse")).to_contain_text("Link has responded with staus 401 and status text Unauthorized")
+
+
+#   Cenário: Usuário clica no link forbidden
+def clicar_elements(page):
+    page.get_by_text("Elements").click()
+
+def abrir_links(page):
+    page.get_by_text("Links", exact=True).click()
+
+def clicar_forbidden(page):
+    page.get_by_role("link", name="Forbidden").click()
+
+def validar_resposta_forbidden(page):
+    expect(page.locator("#linkResponse")).to_contain_text("Link has responded with staus 403 and status text Forbidden")
+
+
+#   Cenário: Usuário clica no link not found
+def clicar_elements(page):
+    page.get_by_text("Elements").click()
+
+def abrir_links(page):
+    page.get_by_text("Links", exact=True).click()
+
+def clicar_not_found(page):
+    page.get_by_role("link", name="Not Found").click()
+
+def validar_resposta_not_found(page):
+    expect(page.locator("#linkResponse")).to_contain_text("Link has responded with staus 404 and status text Not Found")
--- steps/links_steps.py
+++ steps/links_steps.py
@@ -1,5 +1,5 @@
 from pytest_bdd import when, then
-from pages.links_page import abrir_links, clicar_created, clicar_elements, clicar_home, validar_resposta_created, validar_resposta_home
+from pages.links_page import abrir_links, clicar_created, clicar_elements, clicar_home, clicar_moved, clicar_no_content, clicar_unauthorized, validar_resposta_bad_request, validar_resposta_created, validar_resposta_forbidden, validar_resposta_home, validar_resposta_moved, validar_resposta_no_content, clicar_moved, validar_resposta_moved, validar_resposta_bad_request, clicar_bad_request, validar_resposta_not_found, validar_resposta_unauthorized, clicar_forbidden, validar_resposta_forbidden, clicar_not_found, validar_resposta_not_found
 from utils.helpers import tirar_print
 
 #  Cenário: Usuário clica no link home
@@ -63,5 +63,139 @@ def step_clicar_created(page):
     tirar_print(page, "clica no link Created")
 
 @then("o usuário deve visualizar Link has responded with staus 201 and status text Created")
-def step_validar_resposta_created(context):
-    validar_resposta_created(context.page)
+def step_validar_resposta_created(page):
+    validar_resposta_created(page)
+    tirar_print(page, "o usuário deve visualizar Link has responded with staus 201 and status text Created")
+
+
+#   Cenário: Usuário clica no link no content
+@when('ele clica na opção Elements')
+def step_clicar_elements(page):
+    clicar_elements(page)
+    tirar_print(page, "ele clica na opção Elements")
+    
+@when('acessa a seção Links')
+def step_abrir_links(page):
+    abrir_links(page)
+    tirar_print(page, "acessa a seção Links")
+
+@when("clica no link No Content")
+def step_clicar_no_content(page):
+    clicar_no_content(page)
+    tirar_print(page, "clica no link No Content")
+
+@then("o usuário deve visualizar Link has responded with staus 204 and status text No Content")
+def step_validar_resposta_no_content(page):
+    validar_resposta_no_content(page)
+    tirar_print(page, "o usuário deve visualizar Link has responded with staus 204 and status text No Content")
+
+
+#   Cenário: Usuário clica no link moved
+@when('ele clica na opção Elements')
+def step_clicar_elements(page):
+    clicar_elements(page)
+    tirar_print(page, "ele clica na opção Elements")
+    
+@when('acessa a seção Links')
+def step_abrir_links(page):
+    abrir_links(page)
+    tirar_print(page, "acessa a seção Links")
+
+@when("clica no link moved")
+def step_clicar_moved(page):
+    clicar_moved(page)
+    tirar_print(page, "clica no link Moved")
+
+@then("o usuário deve visualizar Link has responded with staus 301 and status text Moved Permanently")
+def step_validar_resposta_moved(page):
+    validar_resposta_moved(page)
+    tirar_print(page, "o usuário deve visualizar Link has responded with staus 301 and status text Moved Permanently")
+
+
+#   Cenário: Usuário clica no link bad request
+@when('ele clica na opção Elements')
+def step_clicar_elements(page):
+    clicar_elements(page)
+    tirar_print(page, "ele clica na opção Elements")
+    
+@when('acessa a seção Links')
+def step_abrir_links(page):
+    abrir_links(page)
+    tirar_print(page, "acessa a seção Links")
+
+@when("clica no link bad request")
+def step_clicar_bad_request(page):
+    clicar_bad_request(page)
+    tirar_print(page, "clica no link Bad Request")
+
+@then("o usuário deve visualizar Link has responded with staus 400 and status text Bad Request")
+def step_validar_resposta_bad_request(page):
+    validar_resposta_bad_request(page)
+    tirar_print(page, "o usuário deve visualizar Link has responded with staus 400 and status text Bad Request")
+
+
+#   Cenário: Usuário clica no link unauthorized
+@when('ele clica na opção Elements')
+def step_clicar_elements(page):
+    clicar_elements(page)
+    tirar_print(page, "ele clica na opção Elements")
+    
+@when('acessa a seção Links')
+def step_abrir_links(page):
+    abrir_links(page)
+    tirar_print(page, "acessa a seção Links")
+
+@when("clica no link unauthorized")
+def step_clicar_unauthorized(page):
+    clicar_unauthorized(page)
+    tirar_print(page, "clica no link Unauthorized")
+    
+@then("o usuário deve visualizar Link has responded with staus 401 and status text Unauthorized")
+def step_validar_resposta_unauthorized(page):
+    validar_resposta_unauthorized(page)
+    tirar_print(page, "o usuário deve visualizar Link has responded with staus 401 and status text Unauthorized")
+
+
+#   Cenário: Usuário clica no link forbidden
+@when('ele clica na opção Elements')
+def step_clicar_elements(page):
+    clicar_elements(page)
+    tirar_print(page, "ele clica na opção Elements")
+    
+@when('acessa a seção Links')
+def step_abrir_links(page):
+    abrir_links(page)
+    tirar_print(page, "acessa a seção Links")
+
+@when("clica no link forbidden")
+def step_clicar_forbidden(page):
+    clicar_forbidden(page)
+    tirar_print(page, "clica no link Forbidden")
+
+@then("o usuário deve visualizar Link has responded with staus 403 and status text Forbidden")
+def step_validar_resposta_forbidden(page):
+    validar_resposta_forbidden(page)
+    tirar_print(page, "o usuário deve visualizar Link has responded with staus 403 and status text Forbidden")
+
+
+#   Cenário: Usuário clica no link not found
+@when('ele clica na opção Elements')
+def step_clicar_elements(page):
+    clicar_elements(page)
+    tirar_print(page, "ele clica na opção Elements")
+    
+@when('acessa a seção Links')
+def step_abrir_links(page):
+    abrir_links(page)
+    tirar_print(page, "acessa a seção Links")
+
+@when("clica no link not found")
+def step_clicar_not_found(page):
+    clicar_not_found(page)
+    tirar_print(page, "clica no link Not Found")
+
+@then("o usuário deve visualizar Link has responded with staus 404 and status text Not Found")
+def step_validar_resposta_not_found(page):
+    validar_resposta_not_found(page)
+    tirar_print(page, "o usuário deve visualizar Link has responded with staus 404 and status text Not Found")
+    
